9eeb4c6ec1a25ea62b6b209892cfc692b6e44bef
Denwiliusz27 <danwielgosz@gmail.com>
2021-04-17 13:15:12 +0200

    add bured background

---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent implements OnInit {
+  options: string[] = ['lekarz', 'pacjent'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(option: string) {
+    console.log(option);
+  }
+}
