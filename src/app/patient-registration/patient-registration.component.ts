d0813cba3d57f6f2ca09701e578a0c95d065fb21
Denwiliusz27 <danwielgosz@gmail.com>
2021-04-25 11:43:19 +0200

    first styles for patient-registration

--- src/app/patient-registration/patient-registration.component.ts
+++ src/app/patient-registration/patient-registration.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
+
+
+interface PatientRegistration {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-patient-registration',
@@ -6,10 +15,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-registration.component.css']
 })
 export class PatientRegistrationComponent implements OnInit {
+  registrationFormGroup = new FormGroup({
+    name: new FormControl('', [
+      Validators.required
+    ]),
+    surname: new FormControl('', [
+      Validators.required
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)
+    ])
+  });
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  register(): void {
+    console.log('zarejestrowano');
+  }
 }
