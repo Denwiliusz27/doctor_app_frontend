1b89ccb5e0d6796bf2e8d18e93cd4be780d06337
Denwiliusz27 <danwielgosz@gmail.com>
2021-04-28 19:08:13 +0200

    add first imputs for doctor (same as for patient)

--- src/app/doctor-registration/doctor-registration.component.ts
+++ src/app/doctor-registration/doctor-registration.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
+
+interface DocrtorRegistration {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-doctor-registration',
@@ -7,9 +15,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorRegistrationComponent implements OnInit {
 
+  submitted = false;
+
+  registrationFormGroup = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.pattern('^[A-Z][a-z]*')
+    ]),
+    surname: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.pattern('^[A-Z][a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.\'-]+')
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern('(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}')
+    ])
+  });
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  get currentFormControls(): {
+    [key: string]: AbstractControl;
+  } {
+    return this.registrationFormGroup.controls;
+  }
+
+  register(): void {
+    this.submitted = true;
+
+    if (this.registrationFormGroup.invalid){
+      return;
+    }
+
+    console.log('elo');
+  }
 }
