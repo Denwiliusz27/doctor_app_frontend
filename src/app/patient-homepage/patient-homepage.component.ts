f3f13bc756d08d12fa0ddeb86dd35aa7246f01d1
Denwiliusz27 <danwielgosz@gmail.com>
2021-06-20 17:24:40 +0200

    add select to patient-homepage

--- src/app/patient-homepage/patient-homepage.component.ts
+++ src/app/patient-homepage/patient-homepage.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-homepage.component.css']
 })
 export class PatientHomepageComponent implements OnInit {
+  cities: string[] = ['Kraków', 'Warszawa', 'Gdańsk'];
 
   constructor() { }
 
